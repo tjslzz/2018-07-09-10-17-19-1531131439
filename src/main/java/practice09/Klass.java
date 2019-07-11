2a79cc3b79e21b17bad2fce2c2c3d184ace63eac
JarryLi <3244188372@qq.com>
2019-07-11 23:32:53 +0800

    finish practice09 klass-plus

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,39 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private Integer number;
+    private Student leader;
+    private List<Student> member = new ArrayList<>();
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getDisplayName(){
+        return number == 0 ? "No Class":"Class "+number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void assignLeader(Student leader) {
+        if(this.member.contains(leader)){
+            this.leader = leader;
+        }
+        else{
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student){
+        this.member.add(student);
+    }
 }
