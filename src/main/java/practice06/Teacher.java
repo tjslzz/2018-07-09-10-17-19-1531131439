83cc7215b6356d1cc1e2d493bcb44c3771992b25
JarryLi <3244188372@qq.com>
2019-07-11 21:10:27 +0800

    finish practice06 teacher

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,26 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+    public Teacher(String name, Integer age) {
+        super(name, age);
+        this.klass = 0;
+    }
+
+    public Teacher(String name, Integer age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        return klass == 0 ? "My name is "+getName()+". I am "+getAge()+" years old. I am a Teacher. I teach No Class.":"My name is "+getName()+". I am "+getAge()+" years old. I am a Teacher. I teach Class "+klass+".";
+    }
 }
