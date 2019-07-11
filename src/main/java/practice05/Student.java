4c0a80e557f44f25cbd98477fbcd30d32b615be3
JarryLi <3244188372@qq.com>
2019-07-11 21:02:43 +0800

    finish practice05 work_plus

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,21 @@
 package practice05;
 
-public class Student {
+public class Student extends Person{
+    private Integer klass;
+    public Student(String name, Integer age,Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        return "My name is "+getName()+". I am "+getAge()+" years old. I am a Student. I am at Class "+klass+".";
+    }
 }
